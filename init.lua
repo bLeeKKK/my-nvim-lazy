94982b7316852d8daf51e749f094c8886a526285
bin6.li <bin6.li@changhong.com>
2024-11-25 09:38:56 +0800

    优化

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 -- vim.loader.enable()
 if vim.g.vscode then
-  -- VSCode extension
+-- VSCode extension
 else
-  require("config.lazy")
+	require("config.lazy")
 
-  -- 基础配置
-  require("core.options")
-  require("core.keymaps")
-  require("core.im-select")
+	-- 基础配置
+	require("core.options")
+	require("core.keymaps")
+	require("core.im-select")
 end
