e769fd5d22bfac9ffd178f0125be97e75dc4e12c
bin6.li <bin6.li@changhong.com>
2025-05-12 09:12:33 +0800

    2025年5月12日------插件仓库版本关系后配置冲突修复

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -78,94 +78,94 @@ return {
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
-    mason_lspconfig.setup_handlers({
-      -- default handler for installed servers
-      function(server_name)
-        lspconfig[server_name].setup({
-          capabilities = capabilities,
-        })
-      end,
-      ["svelte"] = function()
-        -- configure svelte server
-        lspconfig["svelte"].setup({
-          capabilities = capabilities,
-          on_attach = function(client, bufnr)
-            vim.api.nvim_create_autocmd("BufWritePost", {
-              pattern = { "*.js", "*.ts" },
-              callback = function(ctx)
-                -- Here use ctx.match instead of ctx.file
-                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-              end,
-            })
-          end,
-        })
-      end,
-      ["graphql"] = function()
-        -- configure graphql language server
-        lspconfig["graphql"].setup({
-          capabilities = capabilities,
-          filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-        })
-      end,
-      ["emmet_ls"] = function()
-        -- configure emmet language server
-        lspconfig["emmet_ls"].setup({
-          capabilities = capabilities,
-          filetypes = {
-            -- "html",
-            -- "typescriptreact",
-            -- "javascriptreact",
-            "css",
-            "sass",
-            "scss",
-            "less",
-            "svelte",
-          },
-        })
-      end,
-      ["lua_ls"] = function()
-        -- configure lua server (with special settings)
-        lspconfig["lua_ls"].setup({
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              -- make the language server recognize "vim" global
-              diagnostics = {
-                globals = { "vim" },
-              },
-              completion = {
-                callSnippet = "Replace",
-              },
-            },
-          },
-        })
-      end,
-      ["rust_analyzer"] = function()
-        -- Rust Analyzer 配置
-        lspconfig["rust_analyzer"].setup({
-          capabilities = capabilities,
-          settings = {
-            ["rust-analyzer"] = {
-              cargo = {
-                allFeatures = true, -- 启用所有的 Cargo 功能
-              },
-              checkOnSave = {
-                command = "clippy", -- 保存时运行 Clippy 检查
-              },
-              diagnostics = {
-                disabled = { "inactive-code" }, -- 关闭某些诊断
-              },
-            },
-          },
-          -- on_attach = function(client, bufnr)
-          --   -- 设置快捷键或其他初始化逻辑
-          --   local opts = { noremap = true, silent = true, buffer = bufnr }
-          --   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-          --   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-          --   vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-          -- end,
-        })
-      end,
-    })
+    -- mason_lspconfig.setup_handlers({
+    --   -- default handler for installed servers
+    --   function(server_name)
+    --     lspconfig[server_name].setup({
+    --       capabilities = capabilities,
+    --     })
+    --   end,
+    --   ["svelte"] = function()
+    --     -- configure svelte server
+    --     lspconfig["svelte"].setup({
+    --       capabilities = capabilities,
+    --       on_attach = function(client, bufnr)
+    --         vim.api.nvim_create_autocmd("BufWritePost", {
+    --           pattern = { "*.js", "*.ts" },
+    --           callback = function(ctx)
+    --             -- Here use ctx.match instead of ctx.file
+    --             client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+    --           end,
+    --         })
+    --       end,
+    --     })
+    --   end,
+    --   ["graphql"] = function()
+    --     -- configure graphql language server
+    --     lspconfig["graphql"].setup({
+    --       capabilities = capabilities,
+    --       filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+    --     })
+    --   end,
+    --   ["emmet_ls"] = function()
+    --     -- configure emmet language server
+    --     lspconfig["emmet_ls"].setup({
+    --       capabilities = capabilities,
+    --       filetypes = {
+    --         -- "html",
+    --         -- "typescriptreact",
+    --         -- "javascriptreact",
+    --         "css",
+    --         "sass",
+    --         "scss",
+    --         "less",
+    --         "svelte",
+    --       },
+    --     })
+    --   end,
+    --   ["lua_ls"] = function()
+    --     -- configure lua server (with special settings)
+    --     lspconfig["lua_ls"].setup({
+    --       capabilities = capabilities,
+    --       settings = {
+    --         Lua = {
+    --           -- make the language server recognize "vim" global
+    --           diagnostics = {
+    --             globals = { "vim" },
+    --           },
+    --           completion = {
+    --             callSnippet = "Replace",
+    --           },
+    --         },
+    --       },
+    --     })
+    --   end,
+    --   ["rust_analyzer"] = function()
+    --     -- Rust Analyzer 配置
+    --     lspconfig["rust_analyzer"].setup({
+    --       capabilities = capabilities,
+    --       settings = {
+    --         ["rust-analyzer"] = {
+    --           cargo = {
+    --             allFeatures = true, -- 启用所有的 Cargo 功能
+    --           },
+    --           checkOnSave = {
+    --             command = "clippy", -- 保存时运行 Clippy 检查
+    --           },
+    --           diagnostics = {
+    --             disabled = { "inactive-code" }, -- 关闭某些诊断
+    --           },
+    --         },
+    --       },
+    --       -- on_attach = function(client, bufnr)
+    --       --   -- 设置快捷键或其他初始化逻辑
+    --       --   local opts = { noremap = true, silent = true, buffer = bufnr }
+    --       --   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    --       --   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    --       --   vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    --       -- end,
+    --     })
+    --   end,
+    -- })
   end,
 }
